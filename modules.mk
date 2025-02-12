5a5117168ce2269f72ac83de8b6ba708d5b4c519
Todd Jonston <johnstontoddc@gmail.com>
2025-02-11 23:03:07 -0700

    Full working mod

--- modules.mk
+++ modules.mk
@@ -1,4 +1,4 @@
-mod_http_fingerprint_mod.la: mod_http_fingerprint_mod.slo
-	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version  mod_http_fingerprint_mod.lo
+mod_http_fingerprint.la: mod_http_fingerprint.slo
+	$(SH_LINK) -rpath $(libexecdir) -module -avoid-version  mod_http_fingerprint.lo
 DISTCLEAN_TARGETS = modules.mk
-shared =  mod_http_fingerprint_mod.la
+shared =  mod_http_fingerprint.la
